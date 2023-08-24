6938b1a301266dc83f5ada2eef0a6a28680e3d11
Pt. Prashant tripathi <26687933+PtPrashantTripathi@users.noreply.github.com>
2023-08-24 23:21:48 +0530

    Apply Material Design styling to Fifteen-Puzzle game (#3)

--- js/Fifteen-Puzzle.js
+++ js/Fifteen-Puzzle.js
@@ -6,126 +6,129 @@ var moves = 0;
 //initial puzzle
 var solved_puzzle = [];
 for (var i = 1; i < G ** 2; i++) {
-    solved_puzzle.push(i);
+  solved_puzzle.push(i);
 }
-solved_puzzle.push(0)
+solved_puzzle.push(0);
 
 //inhearted function
 var canBoardWin = (array) => {
-    // Check if Start board is the same after ramdomize
-    let startBoardPosition = array.every((el, i) => {
-        return el === solved_puzzle[i]
-    });
-    if (startBoardPosition) return false;
+  // Check if Start board is the same after ramdomize
+  let startBoardPosition = array.every((el, i) => {
+    return el === solved_puzzle[i];
+  });
+  if (startBoardPosition) return false;
 
-    // Check can board win
-    let p = 0;
-    let row = 0;
-    let blankRow = 0;
-    for (let i = 0; i < array.length; i++) {
-        if (i % G == 0) row++;
-        if (array[i] == 0) {
-            blankRow = row;
-            continue;
-        }
-        for (let j = i + 1; j < array.length; j++) {
-            if (array[i] > array[j] && array[j] != 0) p++;
-        }
+  // Check can board win
+  let p = 0;
+  let row = 0;
+  let blankRow = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (i % G == 0) row++;
+    if (array[i] == 0) {
+      blankRow = row;
+      continue;
     }
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] > array[j] && array[j] != 0) p++;
+    }
+  }
 
-    if (G % 2 == 0 && blankRow % 2 != 0) return p % 2 != 0;
-    else return p % 2 == 0;
-}
+  if (G % 2 == 0 && blankRow % 2 != 0) return p % 2 != 0;
+  else return p % 2 == 0;
+};
 
 //jumble puzzle
 var jumble = () => {
-    let array = solved_puzzle.concat().sort(() => Math.random() - 0.5);
-    if (canBoardWin(array)) {
-        return array;
-    }
-    return jumble();
-}
+  let array = solved_puzzle.concat().sort(() => Math.random() - 0.5);
+  if (canBoardWin(array)) {
+    return array;
+  }
+  return jumble();
+};
 
 var A = jumble();
 
 //switch the local
 var switcher = (l) => {
-    moves++;
-    let temp = A[l[0]];
-    A[l[0]] = A[l[1]];
-    A[l[1]] = temp;
-    printer();
-}
+  moves++;
+  let temp = A[l[0]];
+  A[l[0]] = A[l[1]];
+  A[l[1]] = temp;
+  printer();
+};
 
 //win checker puzzle
 var checker = () => {
-    let flag = true;
-    A.forEach((a, i) => (flag *= a == (i == A.length - 1 ? 0 : i + 1)));
-    if (flag) {
-        var msg = document.getElementById("msg");
-        msg.textContent = "you won";
-        msg.setAttribute("style", "color:gold;font-size:x-large;font-weight:bold");
-    }
-}
+  let flag = true;
+  A.forEach((a, i) => (flag *= a == (i == A.length - 1 ? 0 : i + 1)));
+  if (flag) {
+    var msg = document.getElementById("msg");
+    msg.textContent = "you won";
+    msg.setAttribute("style", "color:gold;font-size:x-large;font-weight:bold");
+  }
+};
 
 //location finder
 var finder = (n) => {
-    let loc = [0, 0];
-    for (let i = 0; i < A.length; i++) {
-        if (A[i] == n) loc[0] = i;
-        else if (A[i] == 0) loc[1] = i;
-    }
-    return loc;
-}
+  let loc = [0, 0];
+  for (let i = 0; i < A.length; i++) {
+    if (A[i] == n) loc[0] = i;
+    else if (A[i] == 0) loc[1] = i;
+  }
+  return loc;
+};
 
 //play
 var play = (n) => {
-    var msg = document.getElementById("msg");
-    msg.innerHTML = "";
-    let l = finder(n);
-    //left-right
-    if (Math.floor(l[0] / G) == Math.floor(l[1] / G) && Math.abs(l[0] - l[1]) == 1) {
-        switcher(l);
-    }
-    //up-down
-    else if (Math.abs(l[0] - l[1]) == G) {
-        switcher(l);
-    }
-    //cant switch
-    else {
-        var error = document.getElementById("msg");
-        error.textContent = `cant move : ${n}`;
-        error.style.color = "red";
-    }
-    checker();
-}
+  var msg = document.getElementById("msg");
+  msg.innerHTML = "";
+  let l = finder(n);
+  //left-right
+  if (
+    Math.floor(l[0] / G) == Math.floor(l[1] / G) &&
+    Math.abs(l[0] - l[1]) == 1
+  ) {
+    switcher(l);
+  }
+  //up-down
+  else if (Math.abs(l[0] - l[1]) == G) {
+    switcher(l);
+  }
+  //cant switch
+  else {
+    var error = document.getElementById("msg");
+    error.textContent = `cant move : ${n}`;
+    error.style.color = "red";
+  }
+  checker();
+};
 
 //print puzzle
 var printer = () => {
-    //moves
-    var tmoves = document.getElementById("moves");
-    tmoves.textContent = `Total moves : ${moves}`;
-    tmoves.style.color = "blue";
-    //puzzle table
-    var table = document.getElementById("puzzle");
-    table.innerHTML = "";
-    var tableBody = document.createElement("TBODY");
-    table.appendChild(tableBody);
-    let p = 0;
-    for (let i = 0; i < G; i++) {
-        var tr = document.createElement("TR");
-        tableBody.appendChild(tr);
-        for (let j = 0; j < G; j++) {
-            var td = document.createElement("TD");
-            if (A[p] > 0) {
-                td.setAttribute("onclick", `play("${A[p]}")`);
-                td.appendChild(document.createTextNode(A[p]));
-            } else {
-                td.appendChild(document.createTextNode(""));
-            }
-            tr.appendChild(td);
-            p++;
-        }
+  //moves
+  var tmoves = document.getElementById("moves");
+  tmoves.textContent = `Total moves : ${moves}`;
+  tmoves.style.color = "blue";
+  //puzzle table
+  var table = document.getElementById("puzzle");
+  table.innerHTML = "";
+  var tableBody = document.createElement("TBODY");
+  table.appendChild(tableBody);
+  let p = 0;
+  for (let i = 0; i < G; i++) {
+    var tr = document.createElement("TR");
+    tableBody.appendChild(tr);
+    for (let j = 0; j < G; j++) {
+      var td = document.createElement("TD");
+      if (A[p] > 0) {
+        td.setAttribute("onclick", `play("${A[p]}")`);
+        td.appendChild(document.createTextNode(A[p]));
+      } else {
+        td.appendChild(document.createTextNode(""));
+      }
+      tr.appendChild(td);
+      p++;
     }
-}
+  }
+};
 printer();
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var path = require('path');
+var path = require("path");
 //var public = path.join(__dirname, 'public');
 var public = path.join(__dirname);
 
 // viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(public, 'index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(public, "index.html"));
 });
 
-app.use('/', express.static(public));
+app.use("/", express.static(public));
 
 let port = process.env.PORT || 8080;
 // Listen
-app.listen(port,  function () {
-  console.log('listening on port:',+ port );
-})
+app.listen(port, function () {
+  console.log("listening on port:", +port);
+});
