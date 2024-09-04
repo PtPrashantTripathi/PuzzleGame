7d8e3e7ece00dd4afc26c8e6a964336da34422fe
Prashant Tripathi <Prashant.Tripathi@unilever.com>
2024-09-04 10:52:51 +0530

    color and minor fixs

--- js/script.js
+++ js/script.js
@@ -13,105 +13,114 @@ const modelElem = document.getElementById("model");
 
 // Start the timer
 function startClock() {
-  if (!timerInterval) {
-    timerInterval = setInterval(() => {
-      clockTime++;
-      // Update the display in seconds
-      clockElem.innerText = clockTime + "s";
-    }, 1000);
-  }
+    if (!timerInterval) {
+        timerInterval = setInterval(() => {
+            clockTime++;
+            // Update the display in seconds
+            clockElem.innerText = clockTime + "s";
+        }, 1000);
+    }
 }
 
 // Reset the timer
 function stopClock() {
-  clearInterval(timerInterval);
-  timerInterval = null;
-  clockTime = 0;
-  clockElem.innerText = "0s";
+    clearInterval(timerInterval);
+    timerInterval = null;
+}
+
+function resetClock() {
+    clockTime = 0;
+    clockElem.innerText = "0s";
 }
 
 // Function to update the DOM based on the game state
 function updateBoard(movedNum) {
-  movesElem.textContent = puzzle.moves;
+    movesElem.textContent = puzzle.moves;
 
-  // Clear the puzzle board before adding new elements
-  boardElem.innerHTML = "";
+    // Clear the puzzle board before adding new elements
+    boardElem.innerHTML = "";
 
-  puzzle.data.forEach((number) => {
-    // Create the outer 'cell' div
-    const cellDiv = document.createElement("div");
-    cellDiv.classList.add("cell");
+    puzzle.data.forEach((number) => {
+        // Create the outer 'cell' div
+        const cellDiv = document.createElement("div");
+        cellDiv.classList.add("cell");
 
-    // Create the inner 'number-cell' div
-    const numberCellDiv = document.createElement("div");
-    numberCellDiv.classList.add("number-cell");
-    numberCellDiv.id = number;
+        // Create the inner 'number-cell' div
+        const numberCellDiv = document.createElement("div");
+        numberCellDiv.classList.add("number-cell");
+        numberCellDiv.id = number;
 
-    // Add the appropriate class based on the number
-    if (number === 0) {
-      numberCellDiv.classList.add("number-cell-empty");
-    }
-    if (puzzle.isCorrect(number)) {
-      numberCellDiv.classList.add("number-cell-correct");
-    }
-
-    // Check if the number should bounce in
-    if (!movedNum || number === movedNum) {
-      numberCellDiv.classList.add("bounceIn");
-      numberCellDiv.addEventListener(
-        "animationend",
-        function handleAnimationEnd() {
-          numberCellDiv.classList.remove("bounceIn");
-          numberCellDiv.removeEventListener("animationend", handleAnimationEnd);
+        // Add the appropriate class based on the number
+        if (number === 0) {
+            numberCellDiv.classList.add("number-cell-empty");
         }
-      );
-    }
+        if (puzzle.isCorrect(number)) {
+            numberCellDiv.classList.add("number-cell-correct");
+        }
 
-    // Set the text content and onclick attribute
-    numberCellDiv.textContent = number;
-    numberCellDiv.setAttribute("onclick", `play(${number})`);
+        // Check if the number should bounce in
+        if (!movedNum || number === movedNum) {
+            numberCellDiv.classList.add("bounceIn");
+            numberCellDiv.addEventListener(
+                "animationend",
+                function handleAnimationEnd() {
+                    numberCellDiv.classList.remove("bounceIn");
+                    numberCellDiv.removeEventListener(
+                        "animationend",
+                        handleAnimationEnd
+                    );
+                }
+            );
+        }
 
-    // Append the inner 'number-cell' div to the outer 'cell' div
-    cellDiv.appendChild(numberCellDiv);
+        // Set the text content and onclick attribute
+        numberCellDiv.textContent = number;
+        numberCellDiv.setAttribute("onclick", `play(${number})`);
 
-    // Append the 'cell' div to the puzzle board
-    boardElem.appendChild(cellDiv);
-  });
+        // Append the inner 'number-cell' div to the outer 'cell' div
+        cellDiv.appendChild(numberCellDiv);
+
+        // Append the 'cell' div to the puzzle board
+        boardElem.appendChild(cellDiv);
+    });
 }
 
 // Function to handle user moves
 function play(number) {
-  if (puzzle.canMove(number)) {
-    startClock(); // Start the clock on the first move
-    puzzle.switcher(number);
-    updateBoard(number);
+    if (puzzle.canMove(number)) {
+        startClock(); // Start the clock on the first move
+        puzzle.switcher(number);
+        updateBoard(number);
 
-    // Check if the puzzle is solved
-    if (puzzle.isSolved()) {
-      stopClock(); // Stop the clock
-      showModel();
+        // Check if the puzzle is solved
+        if (puzzle.isSolved()) {
+            showModel();
+            stopClock(); // Stop the clock
+        }
+    } else {
+        const numberCellDiv = document.getElementById(number);
+        // Remove and add 'jiggle' class for the clicked number
+        numberCellDiv.classList.add("jiggle");
+        numberCellDiv.addEventListener(
+            "animationend",
+            function handleAnimationEnd() {
+                numberCellDiv.classList.remove("jiggle");
+                numberCellDiv.removeEventListener(
+                    "animationend",
+                    handleAnimationEnd
+                );
+            }
+        );
     }
-  } else {
-    const numberCellDiv = document.getElementById(number);
-    // Remove and add 'jiggle' class for the clicked number
-    numberCellDiv.classList.add("jiggle");
-    numberCellDiv.addEventListener(
-      "animationend",
-      function handleAnimationEnd() {
-        numberCellDiv.classList.remove("jiggle");
-        numberCellDiv.removeEventListener("animationend", handleAnimationEnd);
-      }
-    );
-  }
 }
 
 function showModel() {
-  modelElem.innerHTML = `
+    modelElem.innerHTML = `
   <div class="modal-wrapper bounceIn">
   <div class="modal-card">
       <div class="modal-container">
-          <div class="text-1">Excellent!</div>
-          <div>It took you <b>${puzzle.moves} moves</b> and <b>${clockTime} seconds</b></div>
+          <div class="text-1">You Won!</div>
+          <div>in <b>${puzzle.moves} moves</b> and <b>${clockTime} seconds</b></div>
           <div><button class="modal-button" onclick="newGame(${puzzle.gridSize})">Play Again</button></div>
       </div>
   </div>
@@ -120,9 +129,11 @@ function showModel() {
 }
 
 function newGame(size) {
-  puzzle = new Puzzle(size);
-  boardElem.style.setProperty("--data-size", size);
-  updateBoard();
+    puzzle = new Puzzle(size);
+    modelElem.innerHTML = "";
+    boardElem.style.setProperty("--data-size", size);
+    resetClock();
+    updateBoard();
 }
 
 // Initialize the board once at the beginning
